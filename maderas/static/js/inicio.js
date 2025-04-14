a7551e9e01492881fe9fc57f4a33e96d2e38e7b6
yahir palacios <cyahirpalacios@gmail.com>
2025-04-14 12:28:56 -0500

    cambio en prooductos y publicidad de productos para separar por tipo al igual que configuraciones menores en otros campos y alteracion en la logica y funcionalidad del boton de accesibilidad

---
+++ maderas/static/js/inicio.js
@@ -0,0 +1,30 @@
+document.getElementById("back-to-top").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  // Funcionalidad sencilla para el carrusel
+  (function() {
+    const carousel = document.getElementById('productosCarousel');
+    const items = carousel.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+
+    const showItem = index => {
+      items.forEach((item, i) => {
+        item.style.display = i === index ? 'block' : 'none';
+      });
+    };
+
+    carousel.querySelector('.prev').addEventListener('click', function() {
+      currentIndex = (currentIndex === 0) ? items.length - 1 : currentIndex - 1;
+      showItem(currentIndex);
+    });
+
+    carousel.querySelector('.next').addEventListener('click', function() {
+      currentIndex = (currentIndex === items.length - 1) ? 0 : currentIndex + 1;
+      showItem(currentIndex);
+    });
+
+    // Inicializa el carrusel
+    showItem(currentIndex);
+  })();

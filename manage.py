28e96b5d170f5e4a3b3edd874454d11cda6dc97b
susana barrera <mariensusanabarrera2005@gmail.com>
2025-04-24 06:52:14 -0500

    cambios stilos de inicio

--- manage.py
+++ manage.py
@@ -12,11 +12,11 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
+            "available on yo ur PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+  
 
 if __name__ == '__main__':
     main()
